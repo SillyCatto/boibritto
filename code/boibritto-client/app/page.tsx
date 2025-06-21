9e7309c1d97c26c6440d2230acd58d9743fbb7f9
Raiyan <raiyanmuhtasim@gmail.com>
2025-06-21 21:45:20 +0600

    removed irrelevant section trending books

--- code/boibritto-client/app/page.tsx
+++ code/boibritto-client/app/page.tsx
@@ -2,14 +2,12 @@
 import Hero from "@/components/home/Hero";
 import Features from "@/components/home/Features";
 import CallToAction from "@/components/home/CallToAction";
-import TrendingBooks from "@/components/home/TrendingBooks";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Hero />
       <Features />
-      <TrendingBooks />
       <CallToAction />
 
     </div>
