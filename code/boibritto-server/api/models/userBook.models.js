4618478095badeb7815541dbceee8c9e608893cd
Raiyan <raiyanmuhtasim@gmail.com>
2025-08-19 22:00:00 +0600

    updated book schema with chapters

--- code/boibritto-server/api/models/userBook.models.js
+++ code/boibritto-server/api/models/userBook.models.js
@@ -1,52 +1,49 @@
-import mongoose from 'mongoose';
-import {  GENRES  } from '../utils/constants.js';
+import mongoose from "mongoose";
+import { GENRES } from "../utils/constants.js";
 
 const userBookSchema = new mongoose.Schema(
-    {
-        author: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        synopsis: {
-            type: String,
-            maxlength: 1000
-        },
-        genres: {
-            type: [String],
-            enum: GENRES,
-            default: []
-        },
-        visibility: {
-            type: String,
-            enum: ["private", "friends", "public"],
-            default: "private"
-        },
-        chapters: [
-            {
-                title: String,
-                content: String, // store Markdown
-                createdAt: {
-                    type: Date,
-                    default: Date.now
-                }
-            }
-        ],
-        coverImage: {
-            type: String
-        },
-        likes: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        }]
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    title: {
+      type: String,
+      required: true,
+      maxlength: 500,
+    },
+    synopsis: {
+      type: String,
+      maxlength: 1000,
+    },
+    genres: {
+      type: [String],
+      enum: GENRES,
+      default: [],
+    },
+    visibility: {
+      type: String,
+      enum: ["private", "public"],
+      default: "private",
+    },
+    coverImage: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export default mongoose.model("UserBook", userBookSchema);
